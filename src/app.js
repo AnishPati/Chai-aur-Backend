cbab82c28120df30566b6362b8ee614b08564ad4
anish <anishpati0325@gmail.com>
2025-09-01 20:22:42 +0530

    util code discussion

--- src/app.js
+++ src/app.js
@@ -1,5 +1,24 @@
 import express from "express";
+import cors from "cors"; // cross origin resorce sharing
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(
+  //app.use is only used to do middileware or config settings
+  //Helps to do setting with cross origin resource sharing
+  cors({
+    origin: process.env.CORS_ORIGIN, //CORS_ORIGIN is used to accept req from any specific url or vercel
+    cradentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" })); //taking data from forms using json and setting it's limits
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //taking data from url
+
+app.use(express.static("public")); //A folder to store some items, images etc...
+
+app.use(cookieParser()); // Getting access of secure cookies from the brwoser by the server secretly and perform crud operations
 
 export { app };
